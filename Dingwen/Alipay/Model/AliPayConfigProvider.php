da8190baf390586ecb33c0ad48afbd6913376696
DINGWEN <18971507137@163.com>
2019-08-06 20:02:22 +0900

    init

---
+++ Dingwen/Alipay/Model/AliPayConfigProvider.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Dingwen\Alipay\Model;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Framework\Escaper;
+use Magento\Payment\Helper\Data as PaymentHelper;
+
+class AliPayConfigProvider implements ConfigProviderInterface
+{
+    /**
+     * @var string[]
+     */
+    protected $methodCode = PCPay::PAYMENT_METHOD_CHECKMO_CODE;
+
+    /**
+     * @var PCPay
+     */
+    protected $method;
+
+    /**
+     * @var Escaper
+     */
+    protected $escaper;
+
+    /**
+     * @param PaymentHelper $paymentHelper
+     * @param Escaper $escaper
+     */
+    public function __construct(
+        PaymentHelper $paymentHelper,
+        Escaper $escaper
+    ) {
+        $this->escaper = $escaper;
+        $this->method = $paymentHelper->getMethodInstance($this->methodCode);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfig()
+    {
+        return $this->method->isAvailable() ? [
+            'payment' => [
+                'pc-pay' => [
+                    ""
+                ],
+            ],
+        ] : [];
+    }
+
+}
